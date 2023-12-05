efb1f001627fef58431d32153edc8190a2bbdcdd
Wiktor <watulewicz@gmail.com>
2023-12-05 23:21:11 +0100

    tests

--- src/components/__tests__/PostItem.test.js
+++ src/components/__tests__/PostItem.test.js
@@ -1,5 +1,5 @@
 import React from "react";
-import { render, screen, fireEvent, waitFor } from "@testing-library/react";
+import { render, screen, fireEvent } from "@testing-library/react";
 import PostItem from "../PostItem";
 import { MemoryRouter, Routes, Route } from "react-router-dom";
 import { createMemoryHistory } from "history";
